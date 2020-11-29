a0a6992423b86aa43295ba621996f0f185ab30c9
Chris Allum <ctallum@gmail.com>
2020-11-29 16:38:14 -0500

    Update files

--- board.py
+++ board.py
@@ -1,2 +1,10 @@
+import sys
+import pygame
+from pygame.locals import *
+
 class Board:
-    pass
+    def __init__(self):
+        WINDOW_SIZE = (600, 400)
+        # create pygame window
+        self._window = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+        pygame.display.set_caption("Magma Boy and Hydro Girl")
--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+"""
+Main file for Magma Boy and Hydro Girl game.
+"""
+
+# import pygame and orther needed libraries
+import sys
+import pygame
+from pygame.locals import *
+
+# import needed classes for game
+from board import Board
+from game import Game
+from players import Players
+from controller import Controller
+
+
 def main():
-    print("Magma Boy and Hydro Girl")
+    # inialize the game
+    clock = pygame.time.Clock()
+    pygame.init()
+
+    # initialize classes
+    board = Board()
+    # main game loop
+    while True:
+        # check for keyboard input
+        for event in pygame.event.get():
+            # if player closes the window
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # update the diaplay
+        pygame.display.update()
+        # run game at rate of 60 fps
+        clock.tick(60)
 
 
 if __name__ == '__main__':
