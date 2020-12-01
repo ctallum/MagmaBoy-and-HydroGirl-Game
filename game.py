685dc852f7de7c41c4a5b12a77e46441b855cda9
Olivia Jo Bradley <obradley@olin.edu>
2020-12-01 11:11:39 -0500

    pseudocode in game.py

--- game.py
+++ game.py
@@ -1,2 +1,14 @@
 class Game:
-    pass
+#     Init
+        # clock/time
+        # Framerate
+        # Score
+        # Level number
+# Def update board
+# Def update characters
+    # Update character position based on acceleration, speed, current location
+# Def kill
+# Def game_over
+# Def pushes (in-game buttons)
+# Def gems
+
