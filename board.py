b0db62847f9cd81df8a63f0338dec7e81dbd8970
Chris Allum <ctallum@gmail.com>
2020-12-01 00:51:18 -0500

    change files a bit

--- board.py
+++ board.py
@@ -2,9 +2,10 @@ import sys
 import pygame
 from pygame.locals import *
 
+
 class Board:
     def __init__(self):
         WINDOW_SIZE = (600, 400)
         # create pygame window
-        self._window = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+        self.screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
         pygame.display.set_caption("Magma Boy and Hydro Girl")
