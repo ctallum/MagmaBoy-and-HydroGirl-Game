be7c7cfd9e5be465f563132f8a57269b8e352232
Chris Allum <ctallum@gmail.com>
2020-12-14 01:45:43 -0500

    temporarily move doors to bottom

--- doors.py
+++ doors.py
@@ -22,32 +22,35 @@ class Doors:
     def get_door(self):
         return self.rect
 
+    def is_door_open(self):
+        return self.door_open
+
     def try_raise_door(self):
-        door_animation_speed = 1.5
+        DOOR_SPEED = 1.5
         if self.player_at_door and not self.door_open:
-            self.door_location = (self.door_location[0], self.door_location[1] - door_animation_speed)
-            self.height_raised += door_animation_speed
+            self.door_location = (self.door_location[0], self.door_location[1] - DOOR_SPEED)
+            self.height_raised += DOOR_SPEED
             if self.height_raised >= 31:
                 self.door_open = True
         elif not self.player_at_door:
             if self.height_raised > 0:
-                self.height_raised -= door_animation_speed
-                self.door_location = (self.door_location[0], self.door_location[1] + door_animation_speed)
+                self.height_raised -= DOOR_SPEED
+                self.door_location = (self.door_location[0], self.door_location[1] + DOOR_SPEED)
                 self.door_open = False
     
 
 class FireDoor(Doors):
     def __init__(self):
-        self.door_location = (64, 48)
-        self.background_location = (64, 48)
-        self.frame_location = (48, 16)
+        self.door_location = (64, 48 + 288)
+        self.background_location = (64, 48+ 288)
+        self.frame_location = (48, 16 + 288)
         self.door_image = pygame.image.load("data/door_images/fire_door.png")
         super().__init__()
 
 class WaterDoor(Doors):
     def __init__(self):
-        self.door_location = (128, 48)
-        self.background_location = (128, 48)
-        self.frame_location = (112, 16)
+        self.door_location = (128, 48 + 288)
+        self.background_location = (128, 48 + 288)
+        self.frame_location = (112, 16 + 288)
         self.door_image = pygame.image.load("data/door_images/water_door.png")
         super().__init__()
