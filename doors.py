1bfe8d39cae741eb922c0f5a59a94d350fce5638
Olivia Jo Bradley <obradley@olin.edu>
2020-12-16 11:53:35 -0500

    Add docstrings to file

--- doors.py
+++ doors.py
@@ -12,22 +12,39 @@ class Doors:
         self.make_rects()
     
     def load_images(self):
+        """
+        Load the images for the door
+        """
         self.frame_image = pygame.image.load("data/door_images/door_frame.png")
         self.frame_image.set_colorkey((255, 0, 255))
         self.door_background = pygame.image.load("data/door_images/door_background.png")
         
 
     def make_rects(self):
+        """
+        Create pygame rect for the door
+        """
         self._rect = pygame.Rect(self.door_location[0], self.door_location[1],
                                 self.door_image.get_width(), self.door_image.get_height())
 
     def get_door(self):
+        """
+        Return a rect containing the location and size of the door
+        """
         return self._rect
 
     def is_door_open(self):
+        """
+        Return a boolean containing the status of the door
+        """
         return self._door_open
 
     def try_raise_door(self):
+        """
+        Try to raise door if conditions are met. 
+
+        Raise door if player is at the door and the door is closed.
+        """
         DOOR_SPEED = 1.5
         if self.player_at_door and not self._door_open:
             self.door_location = (self.door_location[0], self.door_location[1] - DOOR_SPEED)
