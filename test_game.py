95fcac4fa68e85a5b3559b57e3f039850129f8ec
Olivia Jo Bradley <obradley@olin.edu>
2020-12-15 21:53:58 -0500

    add second set of unit tests

--- test_game.py
+++ test_game.py
@@ -7,6 +7,7 @@ from pygame.locals import *
 
 # import classes
 from game import Game
+from doors import FireDoor, WaterDoor
 
 #characters at start
 magma_boy = pygame.Rect(16,350,16,32)
@@ -64,16 +65,38 @@ collision_cases = [
 def test_collision(player,tile,hit_list):
     assert Game.collision_test(player, tile) == hit_list
 
-
+#both doors closed
 fire_door = FireDoor((64, 48), (64, 48), (48, 16))
+water_door = WaterDoor((128, 48), (128, 48), (112, 16))
+
+# both doors open
+fire_door_both = FireDoor((16,350), (16,350), (0, 318))
+fire_door_both._door_open = True
+water_door_both = WaterDoor((16,350), (16,350), (0, 318))
+water_door_both._door_open = True
+
+#fire door open, water door closed
+fire_door_magma = FireDoor((16,350), (16,350), (0, 318))
+fire_door_magma._door_open = True
+water_door_magma = WaterDoor((128, 48), (128, 48), (112, 16))
+
+#water door open, fire door closed
+fire_door_hydro = FireDoor((64, 48), (64, 48), (48, 16))
+water_door_hydro = WaterDoor((16,350), (16,350), (0, 318))
+water_door_hydro._door_open = True
 
 level_done_cases = [
-#magma boy not in front
-([fire_door], False)
+#players not in front of door
+([fire_door, water_door], False),
+#both players in front of door
+([fire_door_both, water_door_both], True),
+#only magmaboy in front of door
+#players not in front of door
+([fire_door_magma, water_door_magma], False),
+#both only hydrogirl in front of door
+([fire_door_hydro, water_door_hydro], False),
 ]
 
 @pytest.mark.parametrize("doors,win_status", level_done_cases)
 def test_level_is_done(doors,win_status):
-    assert Game.level_is_done(doors) == win_status
-
-
+    assert Game.level_is_done(doors,doors) == win_status
